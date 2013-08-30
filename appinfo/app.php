15c94b17c4fd2a778a9c7f3546bf89ced87fc4d3
Ryan Sullivan <kayoticsully@gmail.com>
2013-08-29 21:49:51 -0400

    Added link to login page

--- appinfo/app.php
+++ appinfo/app.php
@@ -31,35 +31,38 @@ use \OCA\AppFramework\Core\API;
 // dont break owncloud when the appframework is not enabled
 if(\OCP\App::isEnabled('appframework')){
 
-	$api = new API('registration');
+    $api = new API('registration');
 
-	// uncomment the next line to activate the admin interface
-	//$api->registerAdmin('admin/settings');
+    // uncomment the next line to activate the admin interface
+    //$api->registerAdmin('admin/settings');
 
-
-	$api->addNavigationEntry(array(
-
-		// the string under which your app will be referenced in owncloud
-		'id' => $api->getAppName('registration'),
-
-		// sorting weight for the navigation. The higher the number, the higher
-		// will it be listed in the navigation
-		'order' => 10,
-
-		// the route that will be shown on startup
-		'href' => $api->linkToRoute('registration_index'),
-
-		// the icon that will be shown in the navigation
-		// this file needs to exist in img/example.png
-		'icon' => $api->imagePath('icon.svg'),
-
-		// the title of your application. This will be used in the
-		// navigation or on the settings page of your app
-		'name' => $api->getTrans()->t('Registration')
-
-	));
+    if( ! $api->isLoggedIn()) {
+        $api->addScript('registration_link');
+    } else if($api->isAdminUser($api->getUserId())) {
+        $api->addNavigationEntry(array(
+            
+            // the string under which your app will be referenced in owncloud
+            'id' => $api->getAppName('registration'),
+            
+            // sorting weight for the navigation. The higher the number, the higher
+            // will it be listed in the navigation
+            'order' => 10,
+            
+            // the route that will be shown on startup
+            'href' => $api->linkToRoute('registration_index'),
+            
+            // the icon that will be shown in the navigation
+            // this file needs to exist in img/example.png
+            'icon' => $api->imagePath('icon.svg'),
+            
+            // the title of your application. This will be used in the
+            // navigation or on the settings page of your app
+            'name' => $api->getTrans()->t('Registration')
+        
+        ));
+    }
 
 } else {
-	$msg = 'Can not enable the  app because the App Framework App is disabled';
-	\OCP\Util::writeLog('registration', $msg, \OCP\Util::ERROR);
+    $msg = 'Can not enable the  app because the App Framework App is disabled';
+    \OCP\Util::writeLog('registration', $msg, \OCP\Util::ERROR);
 }
